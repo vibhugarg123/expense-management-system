a60c8e632fcc23401c98d7f7989bdcc7bb21e070
Vibhu Garg <vibhugarg@Vibhus-MacBook-Air.local>
2025-07-22 15:10:14 +0530

    [slice] Added global exception handler

--- src/main/java/com/slice/demoproject/aspects/GlobalExceptionHandler.java
+++ src/main/java/com/slice/demoproject/aspects/GlobalExceptionHandler.java
@@ -1,10 +1,19 @@
 package com.slice.demoproject.aspects;
 
+import com.slice.demoproject.exceptions.InvalidRequestException;
+import com.slice.demoproject.exceptions.ResourceNotFoundException;
 import java.util.*;
 import org.springframework.http.*;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.MissingPathVariableException;
+import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -28,6 +37,80 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, errors.toString());
     problemDetail.setTitle(getTitleFromExceptionClass(ex));
 
+    return new ResponseEntity<>(problemDetail, HttpStatus.BAD_REQUEST);
+  }
+
+  @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+  @ResponseStatus(HttpStatus.PRECONDITION_FAILED)
+  public ProblemDetail handleMethodArgumentTypeMismatchException(
+      MethodArgumentTypeMismatchException ex) {
+    ProblemDetail problemDetail =
+        ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    problemDetail.setTitle(getTitleFromExceptionClass(ex));
+    return problemDetail;
+  }
+
+  @ExceptionHandler(InvalidRequestException.class)
+  ProblemDetail handleInvalidRequestException(InvalidRequestException ex) {
+    ProblemDetail problemDetail =
+        ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    problemDetail.setTitle(getTitleFromExceptionClass(ex));
+    return problemDetail;
+  }
+
+  @ExceptionHandler(ResourceNotFoundException.class)
+  ProblemDetail handleResourceNotFoundException(InvalidRequestException ex) {
+    ProblemDetail problemDetail =
+        ProblemDetail.forStatusAndDetail(HttpStatus.NOT_FOUND, ex.getMessage());
+    problemDetail.setTitle(getTitleFromExceptionClass(ex));
+    return problemDetail;
+  }
+
+  @Override
+  protected ResponseEntity<Object> handleMissingPathVariable(
+      MissingPathVariableException ex,
+      HttpHeaders headers,
+      HttpStatusCode status,
+      WebRequest request) {
+    ProblemDetail problemDetail =
+        ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    problemDetail.setTitle(getTitleFromExceptionClass(ex));
+    return new ResponseEntity<>(problemDetail, HttpStatus.BAD_REQUEST);
+  }
+
+  protected ResponseEntity<Object> handleBindException(
+      BindException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+    List<String> errorList =
+        ex.getBindingResult().getFieldErrors().stream()
+            .map(
+                error ->
+                    String.format("Field [%s] %s", error.getField(), error.getDefaultMessage()))
+            .toList();
+    ProblemDetail problemDetail =
+        ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, errorList.toString());
+    problemDetail.setTitle(getTitleFromExceptionClass(ex));
+    return new ResponseEntity<>(problemDetail, HttpStatus.BAD_REQUEST);
+  }
+
+  protected ResponseEntity<Object> handleHttpMessageNotReadable(
+      HttpMessageNotReadableException ex,
+      HttpHeaders headers,
+      HttpStatusCode status,
+      WebRequest request) {
+    ProblemDetail problemDetail =
+        ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    problemDetail.setTitle(getTitleFromExceptionClass(ex));
+    return new ResponseEntity<>(problemDetail, HttpStatus.BAD_REQUEST);
+  }
+
+  protected ResponseEntity<Object> handleMissingServletRequestParameter(
+      MissingServletRequestParameterException ex,
+      HttpHeaders headers,
+      HttpStatusCode status,
+      WebRequest request) {
+    ProblemDetail problemDetail =
+        ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, ex.getMessage());
+    problemDetail.setTitle(getTitleFromExceptionClass(ex));
     return new ResponseEntity<>(problemDetail, HttpStatus.BAD_REQUEST);
   }
 
